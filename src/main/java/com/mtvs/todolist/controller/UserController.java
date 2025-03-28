756c5fabee6ae84437ded100cb16ab80f754508f
www-author <dream970215@naver.com>
2025-03-28 18:16:08 +0900

    feat : UserController

--- src/main/java/com/mtvs/todolist/controller/UserController.java
+++ src/main/java/com/mtvs/todolist/controller/UserController.java
@@ -1,17 +1,31 @@
 package com.mtvs.todolist.controller;
 
-import com.mtvs.todolist.view.RootView;
+import com.mtvs.todolist.dto.request.UserRequest;
+import com.mtvs.todolist.global.util.Screen;
+import com.mtvs.todolist.global.util.TimeManager;
+import com.mtvs.todolist.service.UserService;
+import com.mtvs.todolist.view.UserView;
 
-public class UserController implements TaskController {
-    // TODO VIEW에서 로직관련 부분 별도 분리.
-    private final RootView rootView;
+public class UserController {
+    private final UserView userView;
+    private final UserService userService;
 
-    public UserController() {
-        this.rootView = RootView.getInstance();
+    private UserController() {
+        this.userView = UserView.getInstance();
+        this.userService = UserService.getInstance();
     }
 
-    @Override
-    public void execute() {
+    public static UserController getInstance() {
+        return new UserController();
+    }
 
+    public void signUp() {
+        this.userView.showSignUpMenu();
+        UserRequest request = this.userView.signUp();
+        this.userService.signUp(request);
+        this.userView.printSignUp();
+        TimeManager.delay(); // 2초 뒤 초기 화면으로 이동
+        Screen.clear();// 기존의 화면을 Clear
+        RootController.getInstance().execute();
     }
 }
