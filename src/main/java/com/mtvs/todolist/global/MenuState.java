d86e4e600d5d4003823a51947e49937613dd432e
www-author <dream970215@naver.com>
2025-03-28 18:14:34 +0900

    rename : 최초 메뉴 클래스 명 변경

--- src/main/java/com/mtvs/todolist/global/MenuState.java
+++ src/main/java/com/mtvs/todolist/global/MenuState.java
@@ -4,14 +4,14 @@ import com.mtvs.todolist.global.error.ErrorCode;
 
 import java.util.Arrays;
 
-public enum Menu {
+public enum MenuState {
     SIGN_UP(1),
     LOGIN(2),
     EXIT(3)
     ;
     private final int order;
 
-    Menu(final int order) {
+    MenuState(final int order) {
         this.order = order;
     }
 
@@ -19,8 +19,8 @@ public enum Menu {
         return this.order;
     }
 
-    public static Menu getMenu(final int order) {
-        return Arrays.stream(Menu.values())
+    public static MenuState getMenu(final int order) {
+        return Arrays.stream(MenuState.values())
                 .filter(x -> x.getOrder() == order)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ErrorCode.INVALID_MENU.getMessage()));
