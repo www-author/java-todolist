36c6fc9b425bd7dee8d862e4d144f63c23de0aec
www-author <dream970215@naver.com>
2025-03-28 18:11:02 +0900

    feat : TimeUnit을 사용한 TimeManager 구현

---
+++ src/main/java/com/mtvs/todolist/global/util/TimeManager.java
@@ -0,0 +1,16 @@
+package com.mtvs.todolist.global.util;
+
+import org.slf4j.event.Level;
+
+import java.util.concurrent.TimeUnit;
+
+public final class TimeManager {
+
+    public static void delay() {
+        try {
+            TimeUnit.SECONDS.sleep(2L);
+        } catch (InterruptedException e) {
+            Log.record(Level.ERROR, e.getMessage());
+        }
+    }
+}
