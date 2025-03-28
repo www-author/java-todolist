82d91b4e4ae14836f0c92a578acf0fe2a5b18f68
www-author <dream970215@naver.com>
2025-03-28 18:18:27 +0900

    feat : Application 코드 정리

--- src/main/java/com/mtvs/todolist/Application.java
+++ src/main/java/com/mtvs/todolist/Application.java
@@ -1,11 +1,8 @@
 package com.mtvs.todolist;
 
-import com.mtvs.todolist.controller.TaskController;
-import com.mtvs.todolist.global.config.JDBCConnection;
+import com.mtvs.todolist.controller.RootController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.sql.Connection;
 
 
 public class Application {
@@ -13,11 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         try {
-            // TODO jdbc connection
-            Connection connection = JDBCConnection.getConnection();
-            // TODO 메인 진입점은 excute 메소드만 호출되고 컨트롤러는 Factory로 생성하여 인터페이스에 각 유형별 캐스팅되로록 구현하기
-            /*TaskController controller = TaskControllerFactory.create();
-            controller.execute();*/
+            RootController.getInstance().execute();
         } catch (Exception e) {
             log.error(e.getMessage());
         }
