80f44f077be086c7b6c9e1b7ccdb2c6d57338b5f
www-author <dream970215@naver.com>
2025-03-28 18:17:45 +0900

    feat : 최초 화면 View

--- src/main/java/com/mtvs/todolist/view/RootView.java
+++ src/main/java/com/mtvs/todolist/view/RootView.java
@@ -1,31 +1,26 @@
 package com.mtvs.todolist.view;
 
 import com.mtvs.todolist.global.Message;
-import com.mtvs.todolist.global.Menu;
-import com.mtvs.todolist.util.Console;
+import com.mtvs.todolist.global.util.Console;
 
-import static com.mtvs.todolist.global.Menu.*;
+import java.util.Scanner;
 
 public class RootView {
-    private RootView() {}
+    private final Scanner scanner;
+
+    private RootView() {
+        this.scanner = Console.open();
+    }
 
     public static RootView getInstance() {
         return new RootView();
     }
 
     public void showMenu() {
-        while (true) {
-            System.out.println(Message.START_MENU.getMessage());
-            Menu menu = getMenu(Console.open().nextInt());
-            switch (menu) {
-                //case SIGN_UP, LOGIN -> // TODO 각자 로직 처리
-                case EXIT -> close();
-                default -> System.out.println(Message.INVALID_MENU.getMessage());
-            }
-        }
+        System.out.println(Message.START_MENU.getMessage());
     }
 
-    private void close() {
+    public void exit() {
         System.out.println(Message.END_MENU.getMessage());
         Console.close();
         System.exit(0);
