8ef8cde3405924504858d14a54eb03d640ae8cfc
www-author <dream970215@naver.com>
2025-03-31 18:33:07 +0900

    feat : UserController 내 호출 메서드 매개변수 추가

--- src/main/java/com/mtvs/todolist/controller/UserController.java
+++ src/main/java/com/mtvs/todolist/controller/UserController.java
@@ -6,24 +6,25 @@ import com.mtvs.todolist.global.util.TimeManager;
 import com.mtvs.todolist.service.UserService;
 import com.mtvs.todolist.view.UserView;
 
+import java.sql.SQLException;
+
 public class UserController {
     private final UserView userView;
     private final UserService userService;
 
-    private UserController() {
+    private UserController() throws SQLException {
         this.userView = UserView.getInstance();
         this.userService = UserService.getInstance();
     }
 
-    public static UserController getInstance() {
+    public static UserController getInstance() throws SQLException {
         return new UserController();
     }
 
     public void signUp() {
         this.userView.showSignUpMenu();
         UserRequest request = this.userView.signUp();
-        this.userService.signUp(request);
-        this.userView.printSignUp();
+        this.userView.printSignUp(this.userService.signUp(request));
         TimeManager.delay(); // 2초 뒤 초기 화면으로 이동
         Screen.clear();// 기존의 화면을 Clear
         RootController.getInstance().execute();
