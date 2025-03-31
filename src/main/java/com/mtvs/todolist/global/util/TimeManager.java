7eaee05031bde1ea0e07bb58e08f64bba6a129fc
www-author <dream970215@naver.com>
2025-03-31 11:25:18 +0900

    fix : log 기록

--- src/main/java/com/mtvs/todolist/global/util/TimeManager.java
+++ src/main/java/com/mtvs/todolist/global/util/TimeManager.java
@@ -6,7 +6,11 @@ import java.util.concurrent.TimeUnit;
 
 public final class TimeManager {
 
-    public static void delay() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(2L);
+    public static void delay()  {
+        try {
+            TimeUnit.SECONDS.sleep(2L);
+        } catch (InterruptedException e) {
+            Log.record(Level.ERROR, e.getMessage());
+        }
     }
 }
