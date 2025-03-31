6fc73e45b2bd922c5290d3e7fe1768c6289eea74
www-author <dream970215@naver.com>
2025-03-31 18:31:21 +0900

    feat : 회원 가입 기능 구현

--- src/main/java/com/mtvs/todolist/service/UserService.java
+++ src/main/java/com/mtvs/todolist/service/UserService.java
@@ -1,15 +1,29 @@
 package com.mtvs.todolist.service;
 
+import com.mtvs.todolist.dao.UserDao;
 import com.mtvs.todolist.dto.request.UserRequest;
+import com.mtvs.todolist.model.User;
+
+import java.sql.SQLException;
 
 public class UserService {
-    public static UserService getInstance() {
+    private final UserDao userDao;
+
+    public UserService() throws SQLException {
+        this.userDao = new UserDao();
+    }
+
+    public static UserService getInstance() throws SQLException {
         return new UserService();
     }
 
-    public void signUp(final UserRequest request) {
-        // TODO 일단, 각 입력 값들의 NULL 체크, 값 비교 (PW & CPW), 이메일 체크는 나중에 구현
+    // todo 특정 이메일 사용자 조회 (EXIST)
 
-        // 별일 없으면 디비 값 저장
+    public boolean signUp(final UserRequest request) {
+        User user = request.toEntity();
+        // TODO 기존 사용자 조회 (중복 이메일 확인)
+        // TODO USER 엔티티 저장
+        // TODO - 1 : 비밀번호 저장 전 암호화
+        return this.userDao.save(user);
     }
 }
