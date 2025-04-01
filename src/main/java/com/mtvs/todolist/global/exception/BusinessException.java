3b5eb86ad413f6da6de1d0f0c3cbbc0ce8c1c68d
www-author <dream970215@naver.com>
2025-04-01 16:01:57 +0900

    feat : BusinessException ErrorCode 타입

--- src/main/java/com/mtvs/todolist/global/exception/BusinessException.java
+++ src/main/java/com/mtvs/todolist/global/exception/BusinessException.java
@@ -1,7 +1,13 @@
 package com.mtvs.todolist.global.exception;
 
+import com.mtvs.todolist.global.error.ErrorCode;
+
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
     }
 }
