9057daf844cc378cb6409cefce2707aa209e9e87
www-author <dream970215@naver.com>
2025-04-01 16:46:37 +0900

    feat : 로그인 뷰

--- src/main/java/com/mtvs/todolist/view/UserView.java
+++ src/main/java/com/mtvs/todolist/view/UserView.java
@@ -1,9 +1,11 @@
 package com.mtvs.todolist.view;
 
-import com.mtvs.todolist.dto.request.UserRequest;
+import com.mtvs.todolist.user.dto.request.LoginRequest;
+import com.mtvs.todolist.user.dto.request.SignUpRequest;
 import com.mtvs.todolist.global.Message;
 import com.mtvs.todolist.global.error.ErrorCode;
 import com.mtvs.todolist.global.util.Console;
+import com.mtvs.todolist.user.dto.response.UserResponse;
 
 public class UserView {
     private UserView() {
@@ -13,15 +15,19 @@ public class UserView {
         return new UserView();
     }
 
-    public void showSignUpMenu() {
-        System.out.println(Message.SIGN_UP_INTRO.getMessage());
+    public void showMenu(String message) {
+        System.out.println(message);
     }
 
     public void printSignUp(boolean isSignUp) {
         System.out.println(isSignUp ? Message.COMPLETE_SIGN_UP.getMessage() : ErrorCode.USER_REGISTRATION_FAILED.getMessage());
     }
 
-    public UserRequest signUp() {
+    public void printLogin(final UserResponse response) {
+        System.out.println(Message.SUCCESS_LOGIN.getMessage() + response.getName() + "님");
+    }
+
+    public SignUpRequest signUp() {
         System.out.flush(); // 이전 콘솔 출력 메시지 초기화를 위헤 호출
         Console.reset();
 
@@ -37,11 +43,24 @@ public class UserView {
         System.out.println(Message.ENTER_CONFIRM_PASSWORD.getMessage());
         String confirmPassword = Console.open().nextLine();
 
-        return UserRequest.of(
+        return SignUpRequest.of(
                 username,
                 email,
                 password,
                 confirmPassword
         );
+    }
+
+    public LoginRequest login() {
+        System.out.flush(); // 이전 콘솔 출력 메시지 초기화를 위헤 호출
+        Console.reset();
+
+        System.out.println(Message.ENTER_YOUR_EMAIL.getMessage());
+        String email = Console.open().nextLine();
+
+        System.out.println(Message.ENTER_YOUR_LOGIN_PASSWORD.getMessage());
+        String password = Console.open().nextLine();
+
+        return LoginRequest.of(email, password);
     }
 }
