94b6107367945ff204b1c3ffa5be87434eb94206
www-author <dream970215@naver.com>
2025-04-01 17:09:13 +0900

    feat : login request dto

---
+++ src/main/java/com/mtvs/todolist/user/dto/request/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.mtvs.todolist.user.dto.request;
+
+public record LoginRequest(
+        String email,
+        String password
+) {
+    public static LoginRequest of(String email, String password) {
+        return new LoginRequest(email, password);
+    }
+}
